5be463ebe5266f06bff706d5a9c619e19c17bcf5
Jan Wijffels <jwijffels@bnosac.be>
2024-05-02 12:16:45 +0200

    let shinyproxy depend on shinyproxy + create group blackbar, user blackbar / add shinyproxy user to group blackbar

--- shinyproxy/entrypoint.sh
+++ shinyproxy/entrypoint.sh
@@ -5,18 +5,28 @@ echo "Checking ${SHINYPROXY_APPLICATION_DIR} for Shinyproxy config file"
 
 # substitute ${ENV_VARIABLE}s with container environment variables
 if [ -f ${SHINYPROXY_APPLICATION_DIR}/application.tmp ]; then
-   echo "  - found application.tmp; interpolating to application.yml"
-   envsubst < ${SHINYPROXY_APPLICATION_DIR}/application.tmp > ${SHINYPROXY_APPLICATION_DIR}/application.yml
+    echo "  - found application.tmp; interpolating to application.yml"
+    envsubst < ${SHINYPROXY_APPLICATION_DIR}/application.tmp > ${SHINYPROXY_APPLICATION_DIR}/application.yml
 fi
 
 # make the shinyproxy configuration available to the application
 if [ -f ${SHINYPROXY_APPLICATION_DIR}/application.yml ]; then
-   ln -s ${SHINYPROXY_APPLICATION_DIR}/application.yml ${SHINYPROXY_INSTALL_DIR}/application.yml
-   echo "  - created symbolic link from ${SHINYPROXY_APPLICATION_DIR}/application.yml to ${SHINYPROXY_INSTALL_DIR}/application.yml"
+    #rm --force ${SHINYPROXY_INSTALL_DIR}/application.yml
+    cp --force ${SHINYPROXY_APPLICATION_DIR}/application.yml ${SHINYPROXY_INSTALL_DIR}/application.yml
+    echo "  - copying ${SHINYPROXY_APPLICATION_DIR}/application.yml to ${SHINYPROXY_INSTALL_DIR}/application.yml in Shinyproxy container"
 else
-   echo "No 'application.yml' or 'application.tmp' was found: defaulting to demo configuration"
+    echo "No 'application.yml' or 'application.tmp' was found: defaulting to demo Shinyproxy configuration"
 fi
 
-#cd ${SHINYPROXY_INSTALL_DIR}
+# copy folder with apps to SHINYPROXY_INSTALL_DIR
+if [ -d ${SHINYPROXY_APPLICATION_DIR}/apps ]; then
+    echo "  - moving all apps from ${SHINYPROXY_APPLICATION_DIR}/apps to ${SHINYPROXY_INSTALL_DIR} in Shinyproxy container"
+    cp --force --recursive ${SHINYPROXY_APPLICATION_DIR}/apps /opt/shinyproxy/blackbar
+    chown -R shinyproxy:blackbar /opt/shinyproxy/blackbar/apps/
+else
+    echo "No 'apps' folder at ${SHINYPROXY_APPLICATION_DIR}"
+fi
+
+echo "Starting up Shinyproxy"
 
 exec "$@"
