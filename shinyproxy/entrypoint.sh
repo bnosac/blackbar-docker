8747fccee8c7ec22c16b2aee44f4c74fe532c883
Jan Wijffels <jwijffels@bnosac.be>
2024-12-13 15:05:17 +0100

    Update default shinyproxy to use openjdk 11, add gettext, rewrite entrypoint, print application.yml at startup, make user blackbar own /opt/shinyproxy

--- shinyproxy/entrypoint.sh
+++ shinyproxy/entrypoint.sh
@@ -1,32 +1,33 @@
 #!/bin/ash
 set -e
 
-echo "Checking ${SHINYPROXY_APPLICATION_DIR} for Shinyproxy config file"
+echo "Checking ${SHINYPROXY_APPLICATION_DIR} for Shinyproxy config file either as application.tmp or as application.yml"
 
-# substitute ${ENV_VARIABLE}s with container environment variables
+# substitute ${ENV_VARIABLE}s in application.tmp with container environment variables
 if [ -f ${SHINYPROXY_APPLICATION_DIR}/application.tmp ]; then
-    echo "  - found application.tmp; interpolating to application.yml"
-    envsubst < ${SHINYPROXY_APPLICATION_DIR}/application.tmp > ${SHINYPROXY_APPLICATION_DIR}/application.yml
+    echo "  - found application.tmp; plugging in environment variables to application.yml"
+    envsubst < ${SHINYPROXY_APPLICATION_DIR}/application.tmp > /opt/shinyproxy/application.yml
 fi
 
-# make the shinyproxy configuration available to the application
+# If file ${SHINYPROXY_APPLICATION_DIR}/application.yml is available, make the shinyproxy configuration available to the application
 if [ -f ${SHINYPROXY_APPLICATION_DIR}/application.yml ]; then
     #rm --force ${SHINYPROXY_INSTALL_DIR}/application.yml
-    cp --force ${SHINYPROXY_APPLICATION_DIR}/application.yml ${SHINYPROXY_INSTALL_DIR}/application.yml
-    echo "  - copying ${SHINYPROXY_APPLICATION_DIR}/application.yml to ${SHINYPROXY_INSTALL_DIR}/application.yml in Shinyproxy container"
+    cp --force ${SHINYPROXY_APPLICATION_DIR}/application.yml /opt/shinyproxy/application.yml
+    echo "  - copying ${SHINYPROXY_APPLICATION_DIR}/application.yml to /opt/shinyproxy/application.yml in Shinyproxy container"
 else
     echo "No 'application.yml' or 'application.tmp' was found: defaulting to demo Shinyproxy configuration"
 fi
 
-# copy folder with apps to SHINYPROXY_INSTALL_DIR
+# If there is an apps folder in the SHINYPROXY_INSTALL_DIR, copy the files over (for development purposes)
 if [ -d ${SHINYPROXY_APPLICATION_DIR}/apps ]; then
     echo "  - moving all apps from ${SHINYPROXY_APPLICATION_DIR}/apps to ${SHINYPROXY_INSTALL_DIR} in Shinyproxy container"
     cp --force --recursive ${SHINYPROXY_APPLICATION_DIR}/apps /opt/shinyproxy/blackbar
     chown -R shinyproxy:blackbar /opt/shinyproxy/blackbar/apps/
 else
-    echo "No 'apps' folder at ${SHINYPROXY_APPLICATION_DIR}"
+    echo "Note: no 'apps' folder at ${SHINYPROXY_APPLICATION_DIR}"
 fi
 
-echo "Starting up Shinyproxy"
+cat /opt/shinyproxy/application.yml
+echo "\nStarting up Shinyproxy\n"
 
 exec "$@"
